0147c1f5735c3af30084aaacdb4fbec9c716be88
mg <nicecorntea@naver.com>
2021-08-21 15:30:23 +0900

    첫 커밋

---
+++ src/main/java/com/example/demo/domain/item/Item.java
@@ -0,0 +1,47 @@
+package com.example.demo.domain.item;
+
+import com.example.demo.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Item {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private int price;
+
+    private int stockQuantity;
+
+
+    // 해당 상품의 정해진 할부 개월수
+    private int loanCount;
+
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+}
