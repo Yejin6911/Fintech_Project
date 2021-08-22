ed6c4538c47db276f207a058e76c8ea01ec904b1
Unknown <cdjin6911@gmail.com>
2021-08-23 06:47:15 +0900

    edit order page

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -8,8 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 public class OrderController {
 
+    @GetMapping("/order/1times")
+    public String disp1TimesBreakDown(){return "order/sendmoney1";}
+
     @GetMapping("/order/3times")
     public String disp3TimesBreakDown(){return "order/sendmoney3";}
+
+    @GetMapping("/order/6times")
+    public String disp6TimesBreakDown(){return "order/sendmoney6";}
 
     @GetMapping("/order/12times")
     public String disp12TimesBreakDown(){return "order/sendmoney12";}
